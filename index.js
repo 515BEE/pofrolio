2cc0e584d360033f4442b7ff160c48aa9337ed57
515BEE <bee515eye@gmail.com>
2022-12-13 13:27:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let name = 'John Smith';
+
+const age = 35;
+
+let score, result, mark;
+
+score = 80;
+
+console.log('My name is ' + name);
+console.log('My age is ' + age);
+
+console.log(result);
+console.log(score);
+
+// Variable notation
+
+// Camel notation
+let mySchool = 'Lautech';
+let numberInStock = 23;
+
+// Pascal notation
+let MySchool = 'FUNAAB';
+
+function add(a, b) {
+  let maxScore = 100;
+
+  console.log(a + b);
+  //   console.log(maxScore);
+
+  function subtract() {
+    console.log(a - b);
+    console.log(MySchool);
+  }
+}
+
+add(12, 22);
+
+subtract();
